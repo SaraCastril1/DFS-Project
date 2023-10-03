a377948742949df220c0960f8c7db9235be998a8
SaraCastril1 <smcastril1@eafit.edu.co>
2023-10-03 15:38:20 -0500

    Read ready

--- api-gateway/src/api2.py
+++ api-gateway/src/api2.py
@@ -14,7 +14,7 @@ config = dotenv_values(".env")
 
 PRODUCER_HOST = config['PRODUCER_HOST']
 PRODUCER_PORT = config['PRODUCER_PORT']
-dn1 = config['DATA_NODE_PORT-1']
+
 
 #ROUNDROBIN
 current_data_node_index = 0
@@ -27,6 +27,8 @@ def get_next_data_node_address():
     current_data_node_index = (current_data_node_index + 1) % len(data_node_addresses)
     return address
 
+
+ #READ FILE!!!!!------------------------------------------------------------------------
 def get_data_node_addresses(filename):
     try:
         with grpc.insecure_channel(f"{PRODUCER_HOST}:{PRODUCER_PORT}") as channel:
@@ -52,6 +54,8 @@ def get_file_from_data_node(data_node_address, filename):
         print(f"Error al recuperar el archivo del DataNode: {error}")
         return None
 
+
+
 @app.route('/readfile', methods=['POST'])
 def readfile_route():
     try:
@@ -63,7 +67,7 @@ def readfile_route():
             return Response('No se pudieron obtener las direcciones de los DataNodes', status=500, content_type='application/json')
 
         my_dataNode = get_next_data_node_address()
-        file_data = get_file_from_data_node(my_dataNode, filename)
+        file_data = get_file_from_data_node(data_node_addresses[0], filename)
 
         if not file_data:
             return Response('Archivo no encontrado en los DataNodes', status=404, content_type='application/json')
@@ -73,6 +77,33 @@ def readfile_route():
     except Exception as error:
         print(f"Error en la solicitud: {error}")
         return Response('Error en la solicitud', status=500, content_type='application/json')
+
+
+
+# TO DO: WRITE FILE!!!!!------------------------------------------------------------------------
+
+def writeFile(filename,folder,file_data,create_folder, data_node_port):
+    try:
+        with grpc.insecure_channel(f"{PRODUCER_HOST}:{data_node_port}",options=[
+        ('grpc.max_receive_message_length', 1024 * 1024 * 100)  
+    ]) as channel:
+            stub = apiGateway_dataNode_pb2_grpc.DataNodeServiceStub(channel)
+            response = stub.WriteFile(apiGateway_dataNode_pb2.WriteFileRequestData(file_data=file_data,filename=filename,folder=folder,create_folder=create_folder))
+            return response
+    except Exception as error:
+        print(error)
+
+
+@app.route('/writefile', methods=['POST'])
+def writefile_route():
+
+    folder = request.form.get('folder')
+    filename = request.files['file_data'].filename
+    file_data = request.files['file_data'].read() 
+    create_folder = request.form.get('create_folder')
+
+    data_node_response = writeFile(filename,folder,file_data,create_folder)
+    return {'succes':data_node_response.write_success}
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
